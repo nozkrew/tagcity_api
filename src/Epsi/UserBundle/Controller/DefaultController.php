13c42a7f8e34b60bdb6f25aaee4415b0c99c3061
nozkrew <thomas.maire1@hotmail.fr>
2017-02-13 10:47:16 +0100

    Initial commit

---
+++ src/Epsi/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Epsi\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('EpsiUserBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Epsi/UserBundle/EpsiUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Epsi\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EpsiUserBundle extends Bundle
+{
+}
